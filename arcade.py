31418d08d980a7c09811034ad0f7111e38df44d8
rwilliams175 <rwilliams175@phm.k12.in.us>
2026-02-27 09:47:56 -0500

    Update arcade.py

--- arcade.py
+++ arcade.py
@@ -1,1 +1,13 @@
-#arcade game file
+import tkinter as tk
+import random
+
+root = tk.Tk()
+root.title("Colosseum Waves")
+root.geometry("1920x1080")
+canvas = tk.Canvas(root, width=1920, height = 1080)
+canvas.pack()
+
+player = canvas.create_rectangle()
+
+
+root.mainloop()
